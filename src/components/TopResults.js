b9b10d75db5b9d80dc7a016001f222820c4651c1
Tomasz Sojka <contact@sojkatomasz.com>
2023-09-13 17:56:27 +0200

    sorting the top results list

--- src/components/TopResults.js
+++ src/components/TopResults.js
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import { firebaseConfig } from "../firebase"
+import ResultsUserContext from "../context/resultsUserContext"
 import axios from "axios"
 import "./styles/topResults.css"
 
@@ -7,6 +8,7 @@ const HTTPS_URL = `${firebaseConfig.databaseURL}/top-results.json`
 
 function TopResults() {
 	const [toggleShow, setToggleShow] = useState(false)
+	const { userName, date, cashWin } = useContext(ResultsUserContext)
 	const [dataUsersResults, setDataUsersResults] = useState()
 
 	useEffect(() => {
@@ -14,12 +16,18 @@ function TopResults() {
 			const res = await axios.get(HTTPS_URL)
 			let allDataUsersResults = []
 			for (const key in res.data) {
-				allDataUsersResults.push({ ...res.data[key], id: key })
+				const cashWinNumber = res.data[key].cashWin
+					.replaceAll(" ", "")
+					.replaceAll("$", "")
+				allDataUsersResults.push({ ...res.data[key], id: key, cashWinNumber })
 			}
+			allDataUsersResults.sort((a, b) => b.cashWinNumber - a.cashWinNumber)
 			setDataUsersResults(allDataUsersResults)
 		}
-		getDataUsersResults()
-	}, [])
+		setTimeout(() => {
+			getDataUsersResults()
+		}, 500)
+	}, [userName, date, cashWin])
 
 	return (
 		<>
@@ -29,36 +37,40 @@ function TopResults() {
 			>
 				{toggleShow ? (
 					<>
-						<i className='fa-solid fa-arrow-up'></i> Top Results
+						<i className='fa-solid fa-arrow-up'></i> Top 10 Results
 					</>
 				) : (
 					<>
-						Top Results <i className='fa-solid fa-arrow-down'></i>
+						Top 10 Results <i className='fa-solid fa-arrow-down'></i>
 					</>
 				)}
 			</button>
 			<div className={`box-top-results ${toggleShow && "animation-box-top-results"}`}>
-				<table className='list-top-results'>
-					<thead>
-						<tr>
-							<th>Ranking</th>
-							<th>Name</th>
-							<th>Date</th>
-							<th>Cash</th>
-						</tr>
-					</thead>
-					{dataUsersResults &&
-						dataUsersResults.map(item => (
-							<tbody>
-								<tr>
-									<th>1</th>
-									<th>{item.userName}</th>
-									<th>{item.date}</th>
-									<th>{item.cashWin}</th>
-								</tr>
-							</tbody>
-						))}
-				</table>
+				{dataUsersResults && dataUsersResults.length ? (
+					<table className='list-top-results'>
+						<thead>
+							<tr>
+								<th>Ranking</th>
+								<th>Name</th>
+								<th>Date</th>
+								<th>Cash</th>
+							</tr>
+						</thead>
+						{dataUsersResults &&
+							dataUsersResults.slice(0, 10).map((item, id) => (
+								<tbody key={item.id}>
+									<tr>
+										<th>{id + 1}</th>
+										<th>{item.userName}</th>
+										<th>{item.date}</th>
+										<th>{item.cashWin}</th>
+									</tr>
+								</tbody>
+							))}
+					</table>
+				) : (
+					<p>No results. Be the first, take up the challenge and play!</p>
+				)}
 			</div>
 		</>
 	)
